d05001ff17940ea651db7b082af5427e9ecc24dd
unknown <rafael_teixeira_1987@hotmail.com>
2020-10-22 17:05:02 -0300

    Add Secao 5

---
+++ Secao_5/ProAgil-App/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
+
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [		
+    AppComponent,
+      EventosComponent,
+      NavComponent
+   ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Secao_5/ProAgil-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit{
+
+  _filtroLista: string;
+  get filtroLista() : string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = value ? this.filtrarEventos(value) : this.eventos;
+  }
+
+  eventosFiltrados: any = [];
+  eventos: any = [];
+  imagemLargura: number = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+  
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){    
+    this.getEventos();
+  }
+
+  alternarImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  filtrarEventos(filtrarPor: string) : any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(e => e.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
+  }
+
+  getEventos(){
+    this.http.get('http://localhost:5000/api/Values').subscribe(
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
+}
